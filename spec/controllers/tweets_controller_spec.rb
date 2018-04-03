c79a27d6d68bba78d5a8d3ddda4a0de221ebe230
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-03 11:04:03 +0900

    Edit file to test "new" action
    example stands for testing action whether it renders the correct view
    file.

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 describe TweetsController do
 
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
   end
 end

66aeb67fe0caf9e9b40ee96e4a09da06d69583d9
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-03 11:43:03 +0900

    Edit file to add test of "index" action
    Line 27 shows that testing array contents with the order according to
    “created_at” descending.
    
    Line 30 check the array contained by tweet instance. Use match matcher
    with sort method.
    
    Line 33~35 testing the view file

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -24,6 +24,15 @@ describe TweetsController do
   end
 
   describe 'GET #index' do
+    it "populates an array of tweets ordered by created_at DESC" do
+      tweets = create_list(:tweet, 3)
+      get :index
+      expect(assigns(:tweets)).to match(tweets.sort{ |a, b| b.created.at <=> a.created.at })
+    end
+
+    it "renders the :index template" do
+      get : index
+      expect(response).to render_template :index
     end
   end
 end
